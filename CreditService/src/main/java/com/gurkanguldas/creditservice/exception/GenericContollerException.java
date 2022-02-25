16280765c8ec9a0289f930b69797c7ef0f2c8f6b
BekirGurkanGuldas <gurkanguldas@gmail.com>
2022-02-26 01:34:45 +0300

    GenericContollerException added.

---
+++ CreditService/src/main/java/com/gurkanguldas/creditservice/exception/GenericContollerException.java
@@ -0,0 +1,36 @@
+package com.gurkanguldas.creditservice.exception;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Log4j2
+public class GenericContollerException
+{
+    //If the entity is empty, the status should be HttpEntity.NOT_FOUND.
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map> handleNotfoundException(EntityNotFoundException exception)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(prepareResponse(exception.getMessage()));
+    }
+
+    //If the entity has a validation error, the status should be HttpEntity.BAD_REQUEST.
+    @ExceptionHandler(EntityValidateException.class)
+    public ResponseEntity<Map> handleNotfoundException2(EntityValidateException exception)
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(prepareResponse(exception.getMessage()));
+    }
+
+    //Mapping Response Messages
+    private Map<String, String> prepareResponse(String message) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", message);
+        return response;
+    }
+}
